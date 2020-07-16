f82c4d70a31c51076440ffa7b5b2fb24cf0a2c85
Richard Werner <s173134@hftl.de>
2020-07-17 00:00:35 +0200

    fenster gui added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import gui_forms.BasicWindow;
+import org.farng.mp3.MP3File;
+
+import javax.swing.*;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args){
-
-    }
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new BasicWindow().getJPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);    }
 }
---
+++ src/main/java/gui_forms/BasicWindow.java
@@ -0,0 +1,13 @@
+package gui_forms;
+
+import javax.swing.*;
+
+public class BasicWindow {
+    private JLabel labelHeader;
+    private JPanel basicWindowView;
+    private JTree tree1;
+
+    public JPanel getJPanel(){
+        return this.basicWindowView;
+    }
+}
