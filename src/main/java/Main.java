86e3db372bc8db597b288629d897d57e02140a78
Richard Werner <s173134@hftl.de>
2020-07-20 14:35:17 +0200

    model started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 import gui.forms.MyJPanel;
 import gui.frame.MyJFrame;
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagConstant;
+import org.farng.mp3.TagException;
+import org.farng.mp3.TagOptionSingleton;
+import org.farng.mp3.id3.AbstractID3v2Frame;
+import org.farng.mp3.id3.AbstractID3v2FrameBody;
+import org.farng.mp3.id3.FrameBodyTALB;
+import org.farng.mp3.id3.ID3v2_4Frame;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
